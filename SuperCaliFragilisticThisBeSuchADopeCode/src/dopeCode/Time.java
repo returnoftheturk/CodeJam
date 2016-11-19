57b8e1046005d92c1327ad74845a89c7bc72dd8c
Ahmet Akgul <akgulahmet@outlook.com>
2016-11-19 12:46:18 -0500

    Create time and course classes

---
+++ SuperCaliFragilisticThisBeSuchADopeCode/src/dopeCode/Time.java
@@ -0,0 +1,26 @@
+package dopeCode;
+
+public class Time {
+	public String day;
+	public String startTime;
+	public String endTime;
+	
+	public Time(String day, String startTime, String endTime){
+		this.day = day;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	public String getDay(){
+		return this.day;
+	}
+	
+	public String getStartTime(){
+		return this.startTime;
+	}
+	
+	public String getEndTime(){
+		return this.endTime;
+	}
+	
+}
